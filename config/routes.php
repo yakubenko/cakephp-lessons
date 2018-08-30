871505c1525e1f04c99eb0969c3d02ee125a5fd7
Stan Yakubenko (HP) <sn.yakubenko@gmail.com>
2018-08-30 10:36:58 +0500

    Commit for a merge

--- config/routes.php
+++ config/routes.php
@@ -79,4 +79,4 @@ Router::scope('/', function (RouteBuilder $routes) {
  * Load all plugin routes. See the Plugin documentation on
  * how to customize the loading of plugin routes.
  */
-Plugin::routes();
+// Plugin::routes();
